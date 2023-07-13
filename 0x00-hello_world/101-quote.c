5bb50376bf551a2c14edeafa427da9fa8b3b1ce8
Relay90 <motaungr511@gmail.com>
2023-07-13 14:02:07 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 
-/** main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by new line, to standard error.
  * Return: 1 (Success)
  */
