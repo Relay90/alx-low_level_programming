775519bf34188612c6a6c5a24d4f34181c493be7
Relay90 <motaungr511@gmail.com>
2023-07-14 13:51:39 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Prints a text according number
+ * Return: Always (Success)
+ *
+ */
+
 int main(void)
+
 {
 	int n;
 
