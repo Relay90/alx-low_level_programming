0bcd976fd1da75b1a948bf62e9e3bd338eecb024
Relay90 <motaungr511@gmail.com>
2023-07-15 14:06:38 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,10 +14,10 @@ int main(void)
 	int d;
 	char low;
 
-	for (d = '0'; d <= '10'; d++)
-		putchar(d);
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
 
-	for (low = 'a'; low <= 'f'; low++
+	for (low = 'a'; low <= 'f'; low++)
 	putchar(low);
 	putchar('\n');
 
