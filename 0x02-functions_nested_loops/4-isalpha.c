bc2bce845bc8e14ccf6fd8efe7a797bd360053c3
Relay90 <motaungr511@gmail.com>
2023-07-25 04:44:38 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic charecter
+ * @c: the charecter to be checked
+ * Return: 1 for alphabetic charecter or 0 for anything else
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
