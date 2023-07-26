035ee2afb93f8cf40d0644d1f5aed27f482f8577
Relay90 <motaungr511@gmail.com>
2023-07-26 13:09:13 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13
+ * @s: check string params
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char alpha1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char alpha2[] = "QWERTYUIOPLKJHGFDSAZXCVBNMmnbvcxzasdfghjklpoiuytrewq";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+		if (s[i] == alpha1[j])
+		{
+			s[i] = alpha2[j];
+			break;
+		}
+		}
+	}
+	return (s);
+}
