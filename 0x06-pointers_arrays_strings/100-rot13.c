8745bd6d9a2ed3d927451d3c925af1f84554ae30
Relay90 <motaungr511@gmail.com>
2023-07-27 12:20:34 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,16 +11,16 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-	char alpha1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char alpha2[] = "QWERTYUIOPLKJHGFDSAZXCVBNMmnbvcxzasdfghjklpoiuytrewq";
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-		if (s[i] == alpha1[j])
+		if (s[i] == data1[j])
 		{
-			s[i] = alpha2[j];
+			s[i] = datarot[j];
 			break;
 		}
 		}
