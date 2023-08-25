3984404b64aee11116f3b6b4c0035d3216e3cf06
Relay90 <motaungr511@gmail.com>
2023-08-25 13:41:30 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print - prints the sentence before the main
+ * function is executed
+ *
+ * Return: 0
+ */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
