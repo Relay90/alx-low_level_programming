898307204ea6e9313116245f642a8cb3a62fdda8
Relay90 <motaungr511@gmail.com>
2023-08-25 13:50:09 -0700

    101-hello_holberton.c

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,22 @@
+section .data
+    hello db "Hello, Holberton", 0   ; Null-terminated string for printf
+
+section .text
+    global main
+    extern printf
+
+main:
+    ; Call printf
+    mov rdi, hello   ; Set the format string
+    xor rax, rax     ; Clear RAX (no floating-point arguments)
+    call printf
+
+    ; Exit program
+    mov rdi, 0       ; Return 0 from main
+    call exit
+
+exit:
+    mov rax, 60      ; syscall number for exit
+    xor rdi, rdi     ; exit status 0
+    syscall
+To compile 
