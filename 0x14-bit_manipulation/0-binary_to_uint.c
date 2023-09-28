4141f6d01498d6bdac3cc261cecbc2d57fdb9e66
Relay90 <motaungr511@gmail.com>
2023-09-28 04:14:10 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	int i;
 	unsigned int result = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (*b != '\0')
+	for (i = 0; b[i]; i++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		result = (result * 2) + (*b - '0');
-		b++;
+		result = 2 * result + (b[i] - '0');
 	}
 
 	return (result);
 }
+
